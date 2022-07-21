4deb3de977b577cd138ad9af0579ed798d597293
Shashank Sai Sangu <sangushashanksai@gmail.com>
2022-07-21 15:25:14 +0530

    Initial Commit

---
+++ IamPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IamPoor
+//
+//  Created by Shashank on 21/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
